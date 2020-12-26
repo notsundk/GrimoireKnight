ced8b2e01470972ea150811e080dbff82d438efc
notsundk <sunpeerawich@gmail.com>
2020-12-26 17:14:23 +0700

    Add Movement

---
+++ Grimoire Knight/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    void Update()
+    {
+        // These inputs are refering to, in Unity: Edit > Project Settings > Input Manager
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character!
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        // Time.fixedDeltaTime ensure that we move the same amount, accross all systems!
+
+        jump = false;
+    }
+}
